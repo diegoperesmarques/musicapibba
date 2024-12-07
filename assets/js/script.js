d8c245caa87505394b7f4c38444d6144919ac06a
diegoperesmarques@gmail.com <102305401+perescmexx@users.noreply.github.com>
2024-12-07 18:18:20 -0300

    Finalizacao layout perguntas

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,10 +1,18 @@
 $("#btn-exercicios").click(function() {
-
-    $("#div-inicial")
-    .animate({ top: '-100px' }, 500)
-    .animate({ top: '0px', opacity: '0' }, 500, function() {
+    $("#div-inicial").css({ display: "block", top: "0px", opacity: '1'})
+    .animate({ top: "100px", opacity: '0', display: "none"}, 500, function() {
         $("#div-inicial").hide();
-        $("#div-exercicios").stop(true, true).slideDown("slow");
+        $("#div-exercicios").css({display: "block", top: "-100px", opacity: '0'})
+        .animate({ top: "0px", opacity: '1'});
     });
+});
 
+
+$(".btn-inicio").click(function() {
+    $("#div-exercicios").css({ display: "block", top: "0px", opacity: '1'})
+    .animate({ top: "100px", opacity: '0', display: "none"}, 500, function() {
+        $("#div-exercicios").hide();
+        $("#div-inicial").css({display: "block", top: "-100px", opacity: '0'})
+        .animate({ top: "0px", opacity: '1'});
+    });
 });
