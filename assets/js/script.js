093a051d7bc1709514a5c36c6a63229eb4b86f63
diegoperesmarques@gmail.com <102305401+perescmexx@users.noreply.github.com>
2024-12-07 19:28:29 -0300

    Add eventos opcoes

--- assets/js/script.js
+++ assets/js/script.js
@@ -8,11 +8,47 @@ $("#btn-exercicios").click(function() {
 });
 
 
+
 $(".btn-inicio").click(function() {
     $("#div-exercicios").css({ display: "block", top: "0px", opacity: '1'})
     .animate({ top: "100px", opacity: '0', display: "none"}, 500, function() {
-        $("#div-exercicios").hide();
-        $("#div-inicial").css({display: "block", top: "-100px", opacity: '0'})
-        .animate({ top: "0px", opacity: '1'});
+        location.reload();
+
     });
+});
+
+
+$(document).on("click", ".opcao", function() {
+
+    let resultado = $(this).data("resultado");
+    let mensagem = $("#txt-resultado");
+
+    if(resultado == "E") {
+        $(this).removeClass("btn-outline-primary").addClass("btn-danger");
+
+        mensagem.removeClass().addClass("alert alert-danger").text("ERROU!!!!");
+    } else {
+        $(this).removeClass("btn-outline-primary").addClass("btn-success");
+
+        mensagem.removeClass().addClass("alert alert-success").text("ACERTOU!!!!");
+
+        $(".opcao").each(function() {
+            if ($(this).data("resultado") === "E") {
+                $(this).removeClass("btn-outline-primary").addClass("btn-danger");
+            }
+        });
+    }
+
+    let piscar = setInterval(function() {
+        mensagem.fadeOut(300).fadeIn(300);
+    }, 600);
+
+
+    setTimeout(function() {
+        clearInterval(piscar);
+        mensagem.stop(true, true).fadeIn(300); 
+    }, 3000);
+
+
+
 });
