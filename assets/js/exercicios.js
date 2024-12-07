d0e3c0741913f8c0f2706ae06dc85b49b343676f
diegoperesmarques@gmail.com <102305401+perescmexx@users.noreply.github.com>
2024-12-07 20:20:30 -0300

    Finalizando interatividade sistema

---
+++ assets/js/exercicios.js
@@ -0,0 +1,41 @@
+let perguntas = [
+    {pergunta: "Quais são as partes da música?", 
+        opcoes: {
+            opcao1: {
+                texto: "Melodia, Harmonia, Rítmo e Contraponto", resposta: true
+            }, 
+            opcao2: {
+                texto: "Melodia, Harmonia", resposta: false
+            }, 
+            opcao3: {
+                texto: "Mú - Si - Ca", resposta: false
+            }
+        }
+    },
+    {pergunta: "Qual o nome do 5º grau da escala maior diatônica?", 
+        opcoes: {
+            opcao1: {
+                texto: "Tônica", resposta: false
+            }, 
+            opcao2: {
+                texto: "Dominante", resposta: true
+            }, 
+            opcao3: {
+                texto: "Sensível", resposta: false
+            }
+        }
+    },
+    {pergunta: "Quais são os semitons naturais?", 
+        opcoes: {
+            opcao1: {
+                texto: "Do e Ré - Mi e Fá", resposta: false
+            }, 
+            opcao2: {
+                texto: "Si e Dó - Ré e Mi", resposta: false
+            }, 
+            opcao3: {
+                texto: "Mi e Fá - Si e Dó", resposta: true
+            }
+        }
+    },
+];
--- assets/js/script.js
+++ assets/js/script.js
@@ -15,19 +15,25 @@ $("#btn-exercicios").click(function() {
     .animate({ top: "100px", opacity: '0', display: "none"}, 500, function() {
         $("#div-inicial").hide();
         $("#div-exercicios").css({display: "block", top: "-100px", opacity: '0'})
-        .animate({ top: "0px", opacity: '1'});
+        .animate({ top: "0px", opacity: '1'}, 200, function() {
+            containerPerguntaRespostas();
+        });
     });
 });
 
 
 
-$(".btn-inicio").click(function() {
+$(document).on("click", ".btn-inicio", function() {
     $("#div-exercicios").css({ display: "block", top: "0px", opacity: '1'})
     .animate({ top: "100px", opacity: '0', display: "none"}, 500, function() {
         recarregar();
     });
 });
 
+
+$(document).on("click", ".btn-proximo", function() {
+    containerPerguntaRespostas();
+});
 
 $(document).on("click", ".opcao", function() {
 
@@ -63,3 +69,65 @@ $(document).on("click", ".opcao", function() {
 
 
 });
+
+
+
+function containerPerguntaRespostas() {
+    $("#div-exercicios").empty();
+
+    let posicao_pergunta = 0;
+
+    if(localStorage.getItem('posicao_pergunta')) {
+        posicao_pergunta = JSON.parse(localStorage.getItem('posicao_pergunta'));
+        posicao_pergunta++;
+    }
+
+    let respostas_recebidas = '';
+    let texto_respostas = '';
+    let texto_pergunta = '';
+    let container = `<button class="btn btn-secondary col-5 btn-inicio">Início</button>`;
+
+    if(posicao_pergunta == 0) {
+
+        localStorage.setItem('posicao_pergunta', JSON.stringify(posicao_pergunta));
+
+        respostas_recebidas = perguntas[0];
+    } else {
+        localStorage.setItem('posicao_pergunta', JSON.stringify(posicao_pergunta));
+        respostas_recebidas = perguntas[posicao_pergunta];
+    }
+
+    if(respostas_recebidas) {
+        texto_pergunta = respostas_recebidas.pergunta;
+
+        for (const [chave, valor] of Object.entries(respostas_recebidas.opcoes)) {
+            texto_respostas += `<button class="btn btn-outline-primary pt-2 pb-2 opcao" 
+            data-resultado="${valor.resposta ? 'C' : 'E'}">${valor.texto}</button>`;
+    
+          }
+    
+        container = `<div class="quiz-container p-4 border rounded shadow-sm">
+                    <h2 class="text-center mb-4">
+                        ${texto_pergunta}
+                    </h2>
+    
+                    <h3 id="txt-resultado" style="text-align: center;">&nbsp;</h3>
+                    <div class="d-grid gap-3">
+                        ${texto_respostas}
+                    </div>
+    
+                    <div class="d-flex justify-content-between mt-3">
+                        <button class="btn btn-secondary ms-auto d-block col-5 btn-inicio">Início</button>
+                        <span class="col-2">&nbsp;</span>
+                        <button class="btn btn-primary ms-auto d-block col-5 btn-proximo">Próximo</button>
+                    </div>
+    
+            </div>`;
+    }
+
+
+
+    $("#div-exercicios").append(container);
+    
+}
+
